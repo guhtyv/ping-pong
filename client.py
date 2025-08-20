cf66d10337a7e71ae092f0f1396df7283a832eb8
guhtyv <133375872+guhtyv@users.noreply.github.com>
2025-08-20 19:26:16 +0300

    add images

--- client.py
+++ client.py
@@ -41,7 +41,9 @@ def receive():
 font_win = font.Font(None, 72)
 font_main = font.Font(None, 36)
 # --- ЗОБРАЖЕННЯ ----
-
+ball_img = transform.scale(image.load("ball.png"), (20, 20))
+player1_img = transform.scale(image.load("paddle.png"), (20, 100))
+player2_img = transform.scale(image.load("paddle.png"), (20, 100))
 # --- ЗВУКИ ---
 
 # --- ГРА ---
@@ -89,9 +91,9 @@ while True:
 
     if game_state:
         screen.fill((30, 30, 30))
-        draw.rect(screen, (0, 255, 0), (20, game_state['paddles']['0'], 20, 100))
-        draw.rect(screen, (255, 0, 255), (WIDTH - 40, game_state['paddles']['1'], 20, 100))
-        draw.circle(screen, (255, 255, 255), (game_state['ball']['x'], game_state['ball']['y']), 10)
+        screen.blit(player1_img, (20, game_state['paddles']['0']))
+        screen.blit(player2_img, (20, game_state['paddles']['1']))
+        screen.blit(ball_img, (game_state['ball']['x']-10, game_state['ball']['y']-10))
         score_text = font_main.render(f"{game_state['scores'][0]} : {game_state['scores'][1]}", True, (255, 255, 255))
         screen.blit(score_text, (WIDTH // 2 -25, 20))
 
